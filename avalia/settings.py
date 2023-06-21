edb1f171523f8ea3dda685f36afd5920a2b2d45a
iamgomes <wgomes100@gmail.com>
2023-06-21 13:07:17 -0400

    fix upload data

--- avalia/settings.py
+++ avalia/settings.py
@@ -182,3 +182,5 @@ EMAIL_HOST_USER = 'wgomes100@gmail.com'
 EMAIL_HOST_PASSWORD = 'jmcfjwsevguhytxu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240 # higher than the count of fields
