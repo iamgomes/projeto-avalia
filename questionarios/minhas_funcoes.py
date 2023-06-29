e6e2bd696da80f2ab486fe53d00a768671d399fb
iamgomes <wgomes100@gmail.com>
2023-06-28 22:09:58 -0400

    altera nome imagem do banco

--- questionarios/minhas_funcoes.py
+++ questionarios/minhas_funcoes.py
@@ -2,6 +2,43 @@ from PIL import Image
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
+import boto3
+from questionarios.models import ImagemEvidencia
+
+
+def alterar_nome_imagem_banco(id_imagem):
+    # Buscar imagem no banco de dados pelo ID
+    imagem = ImagemEvidencia.objects.get(id=id_imagem)
+    
+    # Obter o nome atual da imagem
+    nome_imagem = imagem.imagem.name
+    
+    # Obter o nome do bucket e a chave da imagem no S3
+    bucket = 'avalia-desenvolvimento'
+    chave = f'static/{nome_imagem}'
+
+    nome_arquivo = imagem.imagem.name.split('/')[1]
+    
+    # Definir o novo nome da imagem
+    novo_nome_imagem = f'static/imagem_evidencia/{nome_arquivo}'
+    
+    # Criar uma nova conexão com o cliente do S3
+    s3_client = boto3.client('s3')
+    
+    # Copiar a imagem com o novo nome no S3
+    s3_client.copy_object(
+        Bucket=bucket,
+        CopySource={'Bucket': bucket, 'Key': chave},
+        Key=novo_nome_imagem
+    )
+    
+    # Excluir a imagem antiga no S3
+    s3_client.delete_object(Bucket=bucket, Key=chave)
+    
+    # Atualizar o nome da imagem no banco de dados
+    imagem.imagem.name = novo_nome_imagem
+    imagem.save()
+
 
 
 def altera_imagem(imagem, resposta):
