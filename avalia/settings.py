a407a3c73a19623fffde631b3925c912eac1a07b
iamgomes <wgomes100@gmail.com>
2023-07-09 00:50:44 -0400

    allowed hosts desenv

--- avalia/settings.py
+++ avalia/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['locahost', '127.0.0.1', 'avalia.herokuapp.com', 'avalia.atricon.org.br', 'www.avalia.atricon.org.br']
+ALLOWED_HOSTS = ['locahost', '127.0.0.1', 'avalia.herokuapp.com', 'avalia-desenv-6040e7c98228.herokuapp.com', 'avalia.atricon.org.br', 'www.avalia.atricon.org.br']
 
 INTERNAL_IPS = ['127.0.0.1']
 
